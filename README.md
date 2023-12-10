34ab6626f00de2623922e07a11f1e8632b4b4dcc
bean-frog <gkieran32855@gmail.com>
2023-12-09 22:18:25 -0800

    erm stuff

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 Theatre oriented sound effect application. Not amazing but it works (probably) <br>
 supposedly lightweight but it uses electron so idk about that <br>
 it runs on my cheap ass thinkpad though so it must be alright 🤷‍♂️ <br>
-written and tested on Linux, probably works on mac or windows because electron
+written and tested on Linux, probably works on mac or windows because electron 
+ ## to use:
+ - probably not gonna build it because its annoying
+ - so you have to run it from source
+ - install npm and nodejs
+ - download and unzip the code
+ - go to the folder where it is
+ - run npm install
+ - run npm run start
+ - uhhhhh enjoy the worst app ever created
--- src/app.js
+++ src/app.js
@@ -26,32 +26,20 @@ function updateSelectOptions() {
     });
   });
 }
-
-
 document.getElementById('updateList').addEventListener('click', fetchData);
 var audioElements = {};
-
-// Get references to the 4 audio elements
 for (var i = 1; i <= 4; i++) {
     audioElements[i] = document.getElementById('audio-' + i);
 }
-
 function playAudio(filename, channel) {
     var channelKey = 'channel' + channel;
     var containerClass = 'channel-' + channel;
-
     if (!audioElements[channelKey]) {
         audioElements[channelKey] = document.createElement('audio');
         audioElements[channelKey].setAttribute('controls', true);
-        
-        // Set the width of the audio element to 300px
-        audioElements[channelKey].style.width = '300px';
+                audioElements[channelKey].style.width = '300px';
         audioElements[channelKey].classList.add('m-4', 'self-center')
-
-        // Get reference to the container element
         var containerElement = document.querySelector('.' + containerClass);
-
-        // Check if the container element exists
         if (containerElement) {
             containerElement.appendChild(audioElements[channelKey]); // Append to the container element
         } else {
